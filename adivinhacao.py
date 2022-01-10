64fb90ddfa7be2007fac82da74345b04f17434e0
fmari <f.mariliafernandes@gmail.com>
2022-01-10 11:59:33 -0300

    melhorias no código

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,18 +6,23 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1,total_de_tentativas+1):
     print(f"Tentativa {rodada} de {total_de_tentativas}")
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou")
+        print("Você acertou!")
+        continue
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto")
