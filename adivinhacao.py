f39c12b84e56523624699b1d637e679fded3abec
fmari <f.mariliafernandes@gmail.com>
2022-01-20 18:28:00 -0300

    implantação de nível de dificuldade

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,19 @@ print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
-rodada = 1
+total_de_tentativas = 0
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1,total_de_tentativas+1):
     print(f"Tentativa {rodada} de {total_de_tentativas}")
@@ -31,6 +42,6 @@ for rodada in range(1,total_de_tentativas+1):
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto")
 
-    rodada = rodada + 1
+
 
 print ("Fim do jogo")
