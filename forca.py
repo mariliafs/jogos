516cfa2fdac71f5d06b8173a6199fbc99a3324cb
fmari <f.mariliafernandes@gmail.com>
2022-02-02 18:50:25 -0300

    começo do jogo da forca

--- forca.py
+++ forca.py
@@ -5,7 +5,25 @@ def jogar():
     print("***Bem vindo no jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index = index + 1
+        print("jogando...")
+
     print ("Fim do jogo")
+
 
 if(__name__ == "__main__"):
     jogar()
